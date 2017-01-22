61fd75eb7f4158913f50c35d7b68a0eff1dce440
Estefania Morton <estefania.morton@gmail.com>
2017-01-21 14:03:44 -1000

    controllers working with players module

--- server.js
+++ server.js
@@ -3,13 +3,40 @@ const app = express();
 const controller = require('./routes/controller');
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({ port: 8081 });
+const players = require('./players.js')
+const MOVE_SPEED = 100;
 
 app.use('/controller', controller);
 
+app.use(express.static('./public'));
+
+// let players = []
+// let id = 0;
+
+const addPlayer = (ws) => {
+  let newPlayer = {
+      id: id,
+      ws: ws,
+      x: 0,
+      y: 0
+    }
+  players.push(newPlayer);
+  id++
+}
+
 wss.on('connection', ((ws) => {
+  let len = players.getLength();
+
+  if(len === 4) {
+    ws.send('sorry too many players');
+  } else {
+    players.addPlayer(ws);
+    ws.send('hello player')
+  }
 
   ws.on('message', (message) => {
-    controlHandler(message);
+    let player = players.getPlayer(ws);
+    controlHandler(player, message);
   });
 
   ws.on('end', () => {
@@ -17,8 +44,35 @@ wss.on('connection', ((ws) => {
   });
 }));
 
-const controlHandler = (msg) => {
-  console.log(`received: ${msg}`);
+const controlHandler = (player, msg) => {
+  console.log(`player ${player.id} pressed: ${msg}`);
+  switch(msg) {
+    case 'up':
+    player.y -= MOVE_SPEED;
+    break;
+
+    case 'down':
+    player.y += MOVE_SPEED;
+    break;
+
+    case 'left':
+    player.x -= MOVE_SPEED;
+    break;
+
+    case 'right':
+    player.x += MOVE_SPEED;
+    break;
+
+    case 'green':
+    player.shoot = true;
+    break;
+
+    case 'red':
+    player.speed = true;
+    break;
+  }
+  console.log(player.x, player.y);
+
 }
 
 module.exports = app;
