893bac536d2f5c2fe74b573483178a53be41728b
ericfune496 <ericfune@hawaii.edu>
2017-01-22 11:46:59 -1000

    Add files via upload
    latest of mines

--- game.js
+++ game.js
@@ -15,21 +15,28 @@
     var platforms;
     var cursors;
     var jumpButton;
-    var widthResize = 25000;
-    var numberOfObstacles = 500;
+    var widthResize = 3000;
+    var numberOfObstacles = 30;
     var randomX;
+    let playerBullets;
+    var greenEnemies;
+    let enemyBullets;
 
-    const preload = _ =>
-    {
+    const PLAYER_BULLET_SPEED = 18;
+    const ENEMY_BULLET_ACCEL = 100;
+
+    const preload = _ => {
         game.stage.backgroundColor = '#85b5e1';
-        game.load.baseURL = 'http://examples.phaser.io/assets/';
+        // load boolets
+        game.load.spritesheet(GFX, 'assets/spritesheet.png', 28, 28);
+        //game.load.baseURL = 'http://examples.phaser.io/assets/';
         game.load.crossOrigin = 'anonymous';
-        game.load.image('player', 'sprites/phaser-dude.png');
-        game.load.image('platform', 'sprites/diamond.png');
+        game.load.image('player', 'http://examples.phaser.io/assets/sprites/phaser-dude.png');
+        game.load.image('platform', 'http://examples.phaser.io/assets/sprites/diamond.png');
+        game.load.image('enemy-green', 'http://examples.phaser.io/assets/sprites/space-baddie.png');
     };
 
-    const create = _ =>
-    {
+    const create = _ => {
         // resize world
         game.world.resize(widthResize, 600);
         console.log("game.width = ", game.width);
@@ -47,22 +54,37 @@
         generateObstacles();
 
         cursors = game.input.keyboard.createCursorKeys();
-        // end template code
+
+        // create bullet things
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        cursors.fire = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+        cursors.fire.onUp.add(handlePlayerFire);
+        playerBullets = game.add.group();
+
+        // create baddies
+        greenEnemies = game.add.group();
+        greenEnemies.enableBody = true;
+        greenEnemies.physicsBodyType = Phaser.Physics.ARCADE;
+        greenEnemies.create(game.world.randomX, game.world.randomY, 'enemy-green');
+        //greenEnemies.createMultiple(25, 'enemy-green');
+        greenEnemies.setAll('anchor.x', 0.5);
+        greenEnemies.setAll('anchor.y', 0.5);
+        greenEnemies.setAll('outOfBoundsKill', true);
+        greenEnemies.setAll('checkWorldBounds', true);
+        launchGreenEnemy();
 
         //   Usually you'd provide a callback to the `game.physics.arcade.collide` function,
         //   which is passed the two sprites involved in the collision, which you can then
         //   perform further processing on. However you can also use this signal:
-        player.body.onCollide = new Phaser.Signal();
-        player.body.onCollide.add(hitSprite, this);
+        //player.body.onCollide = new Phaser.Signal();
+        //player.body.onCollide.add(handleCollisions, this);
 
         // camera will always follow player
         //game.camera.follow(player);
 
     };
 
-
-    const update = _ =>
-    {
+    const update = _ => {
         game.physics.arcade.collide(player, platforms);
 
         // camera is always moving forward at cameraSpeed
@@ -71,28 +93,23 @@
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
 
-        if (cursors.left.isDown)
-        {
+        if (cursors.left.isDown) {
             player.body.velocity.x = -movespeed;
         }
-        else if (cursors.right.isDown)
-        {
+        else if (cursors.right.isDown) {
             player.body.velocity.x = movespeed;
         }
-        else if (cursors.up.isDown)
-        {
+        else if (cursors.up.isDown) {
             player.body.velocity.y = -movespeed;
         }
-        else if (cursors.down.isDown)
-        {
+        else if (cursors.down.isDown) {
             player.body.velocity.y = movespeed;
         }
 
         //wrap god
         //We do a little math to determine whether the game world has wrapped around.
         //If so, we want to destroy everything and regenerate, so the game will remain random
-        if (!wrapping && player.x < game.width)
-        {
+        if (!wrapping && player.x < game.width) {
             console.log("wrapping");
             console.log("player.x =", player.x);
             //We only want to destroy and regenerate once per wrap, so we test with wrapping var
@@ -103,13 +120,11 @@
             justWrapped = true;
         }
        //else if (player.x >= game .width)
-        else if (player.x >= (widthResize - 200))
-        {
+        else if (player.x >= (widthResize - 200)) {
             console.log("else if player.x =", player.x, ">=", widthResize - 200, ", wrapping=", wrapping);
             wrapping = false;
         }
-        else
-        {
+        else {
             //console.log("else player.x =", player.x);
             justWrapped = false;
         }
@@ -118,13 +133,45 @@
         //wrapping, rather than going to the end of the screen first.
         game.world.wrap(player, -200, false, true, false);
 
+        handleBulletAnimations();
+        handleBulletCollisions();
+        //game.physics.arcade.collide(player, platforms, handleCollisions);
+
         // check if out of camera bounds
         if (!player.inWorld && !justWrapped) playerOutOfBounds(player);
+        //launchGreenEnemy();
 
     };
 
-    function generateObstacles()
-    {
+    function launchGreenEnemy() {
+        for (i = 0; i < numberOfObstacles  ; i++) {
+            //game.add.sprite(game.world.randomX, game.world.randomY, 'diamond');
+            //platforms.create(game.world.randomX - (game.width), game.world.randomY, 'platform');
+            greenEnemies.create(game.world.randomX, game.world.randomY, 'enemy-green');
+        };
+    };
+
+    const handlePlayerFire = _ => {
+        playerBullets.add(game.add.sprite(player.x, player.y, GFX, 7));
+    };
+
+    const handleBulletAnimations = _ => {
+        playerBullets.children.forEach(bullet => bullet.x += PLAYER_BULLET_SPEED);
+        //enemyBullets.children.forEach(bullet => {
+        //    game.physics.arcade.accelerateToObject(bullet, player, ENEMY_BULLET_ACCEL);
+        //});
+    };
+
+    // do this when collide happens
+    function handleBulletCollisions() {
+        let enemiesHit = greenEnemies.children.filter(enemy => playerBullets.children.some(bullet => bullet.overlap(enemy)));
+        enemiesHit.forEach(enemy => enemy.destroy());
+        //console.log("collision");
+        //playerBullets.children.filter(bullet => greenEnemies.children.)
+
+    };
+
+    function generateObstacles() {
         console.log("generate");
         platforms = game.add.group();
 
@@ -132,17 +179,15 @@
         platforms.enableBody = true;
 
         // create platforms
-        for (i = 0; i < numberOfObstacles  ; i++)
-        {
+        for (i = 0; i < numberOfObstacles  ; i++) {
             //game.add.sprite(game.world.randomX, game.world.randomY, 'diamond');
             //platforms.create(game.world.randomX - (game.width), game.world.randomY, 'platform');
             platforms.create(randomfix(game.world.randomX), game.world.randomY, 'platform');
-        }
+        };
         platforms.setAll('body.immovable', true);
 
-        // dont have platforms at beginning or end of area
-        function randomfix(randomX)
-        {
+        // dont have platforms at beginning or end of loop
+        function randomfix(randomX) {
             if (randomX >= (widthResize - game.width)) return randomX - game.width;
             else if (randomX < game.width) return randomX + game.width;
             else return randomX;
@@ -150,18 +195,11 @@
     };
 
     // do this when player out of bounds
-    function playerOutOfBounds(player)
-    {
+    function playerOutOfBounds(player) {
         console.log("player OOB");
         justWrapped = false;
         player.kill();
         game.destroy();
-    };
-
-    // do this when collide happens
-    function hitSprite(sprite1, sprite2) {
-        console.log("collision");
-        //sprite1.angle += 5;
     };
 
     // reference for game instantiated here
@@ -169,34 +207,3 @@
 
 
 })(window.Phaser);
-
-//var lilyPads;
-//// group
-//function create()
-//{
-//    lilyPads = game.add.group();
-
-//    // create in a for loop, whatever  
-//    var lily = lilyPads.create(Math.random() * game.width, Math.random() * game.height, 'lilypad', 0);
-//    lily.events.onOutOfBounds.add(lilyPadOOB, this);
-//    lily.checkWorldBounds = true;
-//}
-
-//// sprite out of boundsfunction
-//lilyPadOOB(lily)
-//{
-//    lily.kill();
-//}
-//// called when you need to spawn a recycled / new spritefunction 
-//spawnLilyPad()
-//{
-//    var lily = lilyPads.getFirstDead();
-//    if (!lily)
-//    {
-//        lily = lilyPads.create(0, 0, 'lilypad');
-//        lily.events.onOutOfBounds.add(lilyPadOOB, this);
-//        lily.checkWorldBounds = true;
-//    }
-//    lily.reset(x, y);
-//    lily.animations.frame = 0;
-//}
