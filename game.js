9c76767184b9063c76dfc58a18b795b81980de8f
Estefania Morton <estefania.morton@gmail.com>
2017-01-22 09:22:22 -1000

    pulled down design

--- game.js
+++ game.js
@@ -17,9 +17,9 @@
     const preload = _ => {
 
         game.stage.backgroundColor = '#85b5e1';
-        game.load.baseURL = 'http://examples.phaser.io/assets/';
-        game.load.crossOrigin = 'anonymous';
-        game.load.image('player', 'sprites/phaser-dude.png');
+        // game.load.baseURL = 'http://examples.phaser.io/assets/';
+        // game.load.crossOrigin = 'anonymous';
+        game.load.image('dolphin', 'blue-dolphin.png');
         game.load.image('platform', 'sprites/platform.png');
     };
 
